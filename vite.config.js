7a8828a5ab3929e5cff6fc7f910db8c5433268ad
Abdulalem <abdulalemalsanabani@gmail.com>
2026-04-25 23:23:11 +0300

    added gh

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/abdulalem-portfolio",
+  base: "/abdulalem-portfolio/",
 });
